f7d51d20c9b7ec15710d432a2f940c50f83c23f5
Rane <raneahonen@gmail.com>
2020-04-10 10:35:11 +0300

    Use all tags by default

--- Reflex.js
+++ Reflex.js
@@ -4,7 +4,9 @@ export default class Reflex {
   constructor(config) {
     this.state = config.state;
     this.observer = {};
-    this.define(config.elements);
+    const tags =
+      "a,abbr,address,area,article,aside,audio,b,base,bdi,bdo,blockquote,body,br,button,canvas,caption,cite,code,col,colgroup,data,datalist,dd,del,details,dfn,dialog,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,head,header,hgroup,hr,html,i,iframe,img,input,ins,kbd,label,legend,li,link,main,map,mark,menu,meta,meter,nav,noscript,object,ol,optgroup,option,output,p,param,picture,pre,progress,q,rp,rt,ruby,s,samp,script,section,select,slot,small,source,span,strong,style,sub,summary,sup,table,tbody,td,template,textarea,tfoot,th,thead,time,title,tr,track,u,ul,var,video,wbr";
+    this.define(config.tags || tags.split(","));
   }
 
   get(path) {
@@ -47,7 +49,6 @@ export default class Reflex {
 
   define(tags) {
     const reflex = this;
-
     tags.forEach((tag) => {
       customElements.define(
         `reflex-${tag}`,
@@ -55,6 +56,11 @@ export default class Reflex {
           constructor() {
             super();
             this.reflex = reflex;
+
+            const validName = `reflex-${this.nodeName.toLowerCase()}`;
+            if (this.getAttribute("is") !== validName) {
+              console.error(`Chance to is="reflex-${validName}"`, this);
+            }
           }
 
           connectedCallback() {
